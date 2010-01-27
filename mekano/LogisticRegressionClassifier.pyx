3ce86525bd07819c4323fb28d3e9c2b1464ec356
Abhimanyu Lad <abhimanyulad@gmail.com>
2010-01-27 18:16:32 -0500

    added asserts

--- mekano/LogisticRegressionClassifier.pyx
+++ mekano/LogisticRegressionClassifier.pyx
@@ -4,10 +4,11 @@ import sys
 
 cdef extern from "math.h":
     double exp(double x)
-    
+
 cdef extern from "LRHelper.h":
     object ctrain(object docs, object labels, object mu, double LAMBDA, int maxiter, double epsilon)
-    
+
+
 class LogisticRegressionClassifier(object):
     """A logistic regression classifier trained using CG. Supports prior mean on 'w'.
      
@@ -21,32 +22,40 @@ class LogisticRegressionClassifier(object):
     
     def __cinit__(self):
         pass
-        
+    
     def __init__(self):
         self.maxiter = 100
         self.epsilon = 1e-6
         self.w = AtomVector()
         self.b = 0.0
     
+    
     def train(self, ds, mu=0.0, LAMBDA=0.1):
         """Train the model.
-         
+          
         ds is a Dataset which contains 'docs' as a list AtomVectors, 
             and 'labels' as a list of true/false objects.
         mu can be a double value, or a list of values (at least as many as #features+1)
         lambda is a double value that controls the strength of the prior.
         """
         
+        # Do some error checking since the C function hardly does any.
+        assert type(ds.docs) is list, "ds.docs must be a list"
+        assert type(ds.labels) is list, "ds.labels must be a list"
+        assert type(mu) in [list, int, float], "mu must be list or a number"
+        assert type(LAMBDA) in [int, float], "LAMBDA must be a number"
         w, b = ctrain(ds.docs, ds.labels, mu, LAMBDA, self.maxiter, self.epsilon)
         i = 0
+
+        self.w = AtomVector()
         for v in w:
             self.w[i+1] = v
             i += 1
         self.b = b
+    
 
     def score(self, av):
         cdef double s = self.b + self.w*av
         return 1.0/(1.0+exp(-s))
+    
 
-        
- 
