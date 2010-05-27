5645985878774b7817b4ae9170020a28fe88face
Abhimanyu Lad <abhimanyulad@gmail.com>
2010-05-27 16:51:05 -0400

    added lr test

---
+++ tests/testlr.py
@@ -0,0 +1,22 @@
+import mekano as mk
+
+lr = mk.LogisticRegressionClassifier(mu=2.0, LAMBDA=.1)
+ds = mk.Dataset()
+a1 = mk.AtomVector()
+a1[1] = 2
+a1[3] = 4.0
+a1[5] = -3
+a2 = mk.AtomVector()
+a2[3] = 8.7
+a2[1] = 1.2
+a2[5] = 1.3
+ds.add(a1, False)
+ds.add(a2, True)
+
+lr.train(ds)
+print lr.w
+print lr.b
+
+import sys
+print sys.getrefcount(lr.w)
+
